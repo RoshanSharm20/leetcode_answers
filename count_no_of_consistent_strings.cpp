782ab0c85d0ae314d681acfd389605f153b41281
roshansharma <roshan.s4d@gmail.com>
2022-08-17 16:01:20 +0530

    main solution added

--- count_no_of_consistent_strings.cpp
+++ count_no_of_consistent_strings.cpp
@@ -3,20 +3,20 @@ class Solution
 public:
     int countConsistentStrings(string allowed, vector<string> &words)
     {
-        int count = words.size();
-        bool valid[26] = {false};
+        int count = words.size(); // first declare all strings as valid
+        bool valid[26] = {false}; // initalising as false
         for (auto &c : allowed)
         {
-            valid[c - 'a'] = true;
+            valid[c - 'a'] = true; // only the allowed string characters are assigned true
         }
         for (auto &wor : words)
         {
             for (auto &all : wor)
             {
-                if (valid[all - 'a'] != true)
+                if (valid[all - 'a'] != true) // check for the characters that are allowed
                 {
                     count--;
-                    break;
+                    break; // break is important so that only once count is reduced when one of the characters in string does not match
                 }
             }
         }
