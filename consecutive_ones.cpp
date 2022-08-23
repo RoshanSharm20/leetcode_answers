508314e0238bdc18d42c19be3ebe95f9b21da50a
roshansharma <roshan.s4d@gmail.com>
2022-08-23 22:11:47 +0530

    main solution added

---
+++ consecutive_ones.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int count = 0;
+        int curr = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+                curr = 0;
+            else
+            {
+                curr++;
+                count = max(curr, count);
+            }
+        }
+        return count;
+    }
+};
