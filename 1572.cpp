b10840656745ae166f85b14e111370c787077057
roshansharma <roshan.s4d@gmail.com>
2022-08-24 14:40:12 +0530

    main solution added

---
+++ 1572.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int diagonalSum(vector<vector<int>> &mat)
+    {
+        int sum = 0;
+        int i = 0;
+        int j = mat.size() - 1;
+        for (auto &it : mat)
+        {
+            if (i != j)
+                sum += it[i] + it[j];
+            if (i == j)
+                sum += it[i];
+            i++;
+            j--;
+        }
+        return sum;
+    }
+};
