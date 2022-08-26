6fbb3932b3faa4f3579dd4a6364b7ab8e733f88e
roshansharma <roshan.s4d@gmail.com>
2022-08-26 13:36:00 +0530

    main solution added

---
+++ 35.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int i = 0;
+        while (i < nums.size())
+        {
+            if (nums[i] == target)
+                return i;
+            if (nums[i] > target)
+                return i;
+            i++;
+        }
+        return i;
+    }
+};
