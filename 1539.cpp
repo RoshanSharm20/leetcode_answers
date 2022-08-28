6abf939abfc0610135e402daf99140f435253ae7
roshansharma <roshan.s4d@gmail.com>
2022-08-28 13:25:06 +0530

    main solution added

---
+++ 1539.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int findKthPositive(vector<int> &arr, int k)
+    {
+        vector<int> v(2001); // considering the maximum size that vector can go according to the constraints
+        v[0] = 0;
+        for (int i = 0; i < arr.size(); ++i)
+        {
+            v[arr[i]] = 1;
+        }
+        int i;
+        for (i = 1; i <= 2000; ++i)
+        {
+            if (v[i] == 0)
+                k--;
+            if (k == 0)
+                break;
+        }
+        return i;
+    }
+};
