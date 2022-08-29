e032e0d726ce4a98902056c35ff78d157516772a
roshansharma <roshan.s4d@gmail.com>
2022-08-29 08:50:14 +0530

    main solution added

--- 349.cpp
+++ 349.cpp
@@ -4,25 +4,30 @@ public:
     vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
     {
         vector<int> ans;
-        set<int> s1;
-        set<int> s2;
-        map<int, int> m;
-        for (int i = 0; i < nums1.size(); ++i)
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0, j = 0;
+        while ((i < nums1.size()) && (j < nums2.size()))
         {
-            s1.insert(nums1[i]); // only unique elements must be present to check
-        }
-        for (int i = 0; i < nums2.size(); ++i)
-        {
-            s2.insert(nums2[i]);
-        }
-        for (auto &val : s1)
-            m[val]++;
-        for (auto &val : s2)
-            m[val]++;
-        for (auto pr : m)
-        {
-            if (pr.second >= 2)
-                ans.push_back(pr.first);
+            if (i > 0 && (nums1[i] == nums1[i - 1]))
+            {
+                i++;
+                continue;
+            }
+            if (nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else if (nums1[i] > nums2[j])
+            {
+                j++;
+            }
+            else
+            {
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
         }
         return ans;
     }
