70a68d49abc836251df7d1424283f130d21edebc
roshansharma <roshan.s4d@gmail.com>
2022-09-21 16:56:08 +0530

    main solution added

---
+++ 771.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int numJewelsInStones(string jewels, string stones)
+    {
+        set<char> s;
+        for (int i = 0; i < jewels.size(); ++i)
+        {
+            s.insert(jewels[i]);
+        }
+        int count = 0;
+        for (int i = 0; i < stones.size(); ++i)
+        {
+            auto it = s.find(stones[i]);
+            if (it != s.end())
+                count++;
+        }
+        return count;
+    }
+};
