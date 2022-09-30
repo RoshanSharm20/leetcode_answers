f688f6d333395db8b45acb5fc369ce5db2ff15ae
roshansharma <roshan.s4d@gmail.com>
2022-09-30 12:46:15 +0530

    main solution added

---
+++ 557.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    string reverseWords(string s)
+    {
+        int i;
+        int start = 0;
+        string str = "";
+        string collect = "";
+        for (i = 0; i < s.size(); ++i)
+        {
+            if (s[i] == ' ')
+            {
+                reverse(collect.begin(), collect.end());
+                str += collect + ' ';
+                collect = "";
+            }
+            else
+            {
+                collect += s[i];
+            }
+        }
+        reverse(collect.begin(), collect.end());
+        str += collect;
+        return str;
+    }
+};
