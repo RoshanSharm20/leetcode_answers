e93ae3061a24147129ab591d6d7e4578649eedc4
Roshan Sharma <roshan.s4d@gmail.com>
2022-10-26 09:02:18 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.10%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,12 @@
 class Solution {
-public://dynamic programmig solution 
-    int count_paths(int i,int j,int m,int n,vector<vector<int>> &dp)
-    {
-        if(i>=m || j>=n)
-            return 0;
-        if(i==m-1 && j==n-1)
-            return 1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        else
-            return dp[i][j]=count_paths(i+1,j,m,n,dp) + count_paths(i,j+1,m,n,dp);
-    }
+public:
     int uniquePaths(int m, int n) 
     {
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        int count=count_paths(0,0,m,n,dp);
-        return count;
+        int N = n+m-2;
+        int r= m-1;
+        double res=1;
+        for(int i=1;i<=r;++i)
+            res = res * (N - r + i)/i;
+        return (int)res;
     }
 };
