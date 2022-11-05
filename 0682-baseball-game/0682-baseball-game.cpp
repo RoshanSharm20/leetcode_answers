35297f35e0798b55dd5d0016bd1491ab17f67a83
Roshan Sharma <roshan.s4d@gmail.com>
2022-11-05 23:32:07 +0530

    Time: 7 ms (70.77%), Space: 8.5 MB (26.57%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) 
+    {
+        stack<int> s;
+        int total = 0;
+        for(int i=0;i<operations.size();++i)
+        {
+            if(operations[i]=="+")
+            {
+                int op1=s.top();
+                s.pop();
+                int op2= s.top();
+                int sum = op1+op2;
+                s.push(op1);
+                s.push(sum);
+            }
+            else if(operations[i]=="D")
+            {
+                int op1 = s.top();
+                s.push(op1*2);
+            }
+            else if(operations[i]=="C")
+                s.pop();
+            else
+            {
+                int x = stoi(operations[i]);
+                s.push(x);
+            }
+        }
+        while(s.size()!=0)
+        {
+            total+=s.top();
+            s.pop();
+        }
+        return total;
+    }
+};
