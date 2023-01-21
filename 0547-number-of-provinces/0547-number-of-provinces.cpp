989679a9a092f1e8625f1d3909bab18374a28432
Roshan Sharma <roshan.s4d@gmail.com>
2023-01-21 10:27:55 +0530

    Time: 20 ms (94.72%), Space: 13.8 MB (59.66%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,82 +1,56 @@
 class Solution {
 public:
-    void dfs(int index,vector<int> nodes[],vector<bool>& visited)
+    vector<int> parent,size;
+    int findUparent(int node)
     {
-        for(int i=0;i<nodes[index].size();++i)
+        if(parent[node]==node)
+            return node;
+        return parent[node]=findUparent(parent[node]);
+    }
+    
+    void unionBySize(int a,int b)
+    {
+        int ult_u=findUparent(a);
+        int ult_v=findUparent(b);
+        if(ult_u==ult_v)
+            return;
+        if(size[ult_v]>size[ult_u])
         {
-            if(!visited[nodes[index][i]])
-            {
-                visited[nodes[index][i]]=true;
-                dfs(nodes[index][i],nodes,visited);
-            }
+            parent[ult_u]=ult_v;
+            size[ult_v]+=size[ult_u];
+        }
+        else
+        {
+            parent[ult_v]=ult_u;
+            size[ult_u]+=size[ult_v];
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) 
     {
-        vector<int> nodes[isConnected.size()+1];
-        vector<bool> visited(isConnected.size()+1,false);
+        parent.resize(isConnected.size(),0);
+        size.resize(isConnected.size(),0);
+        for(int i=0;i<parent.size();++i)
+        {
+            parent[i]=i;
+            size[i]=1;
+        }
+        
         for(int i=0;i<isConnected.size();++i)
         {
-            for(int j=0;j<isConnected[0].size();++j)
+            for(int j=0;j<isConnected.size();++j)
             {
                 if(isConnected[i][j]==1)
                 {
-                    nodes[i+1].push_back(j+1);
-                    nodes[j+1].push_back(i+1);
+                    unionBySize(i,j);
                 }
             }
         }
         int count=0;
-        for(int i=1;i<visited.size();++i)
+        for(int i=0;i<parent.size();++i)
         {
-           if(!visited[i])
-           {
-               visited[i]=true;
-               count++;
-               dfs(i,nodes,visited);
-           }
+            if(parent[i]==i)
+                count++;
         }
         return count;
     }
 };
-// class Solution {
-// public:
-//     void dfs(int i,vector<int> nodes[],vector<bool>& visited)
-//     {
-//         for(int j=0;j<nodes[i].size();++i)
-//         {
-//             if(!visited[nodes[i][j]])
-//             {
-//                 visted[nodes[i][j]]=true;
-//                 dfs(nodes[i][j],nodes,visited);
-//             }
-//         }
-//     }
-//     int findCircleNum(vector<vector<int>>& isConnected) 
-//     {
-//         int count=0;
-//         vector<bool> visited(isConnected[0].size(),false);
-//         vector<int> nodes(isConnected.size()+1);
-//         for(int i=0;i<isConnected.size();++i)
-//         {
-//             for(int j=0;j<isConnected[0].size();++j)
-//             {
-//                 if(isConnected[i][j]==1)
-//                 {
-//                     nodes[i+1].push_back(j+1);
-//                     nodes[j+1].push_back(i+1);
-//                 }
-//             }
-//         }
-//         for(int i=1;i<visited.size();++i)
-//         {
-//             if(!visited[i])
-//             {
-//                 count++;
-//                 visited[i]=true;
-//                 dfs(i,nodes,visited);
-//             }
-//         }
-//         return count;
-//     }
-// };
