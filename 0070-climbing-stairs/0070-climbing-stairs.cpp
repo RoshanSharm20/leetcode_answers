4a4a01d2d98bbba35f2b215cc529093556db880f
Roshan Sharma <roshan.s4d@gmail.com>
2023-01-26 09:04:04 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    set<string> ans;
     int dp[46];
     int solution(int n)
     {
-        if(n==0||n==1||n==2){
-            return n;
-        }
-        else if(dp[n]!=-1){
+        if(n==0 || n==1)
+            return 1;
+        if(dp[n]!=-1)
             return dp[n];
-        }
-        else{
-            return dp[n] = solution(n-1)+solution(n-2);
-        }
+        int left = solution(n-1);
+        int right = solution(n-2);
+        return dp[n]=left+right;        
     }
     int climbStairs(int n) 
     {
-        // solution("",n);
         memset(dp,-1,sizeof(dp));
         return solution(n);
     }
