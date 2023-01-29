6fd928d5beecdc980ebd66333a35c22b12570f47
Roshan Sharma <roshan.s4d@gmail.com>
2023-01-29 10:45:09 +0530

    Time: 3 ms (81.39%), Space: 8 MB (15.80%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,3 +1,4 @@
+//tabulation
 class Solution {
 public:
     int solution(int row,int col,vector<vector<int>>& Grid,vector<vector<int>>& dp)
@@ -20,6 +21,28 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& Grid) 
     {
         vector<vector<int>> dp(Grid.size(),vector<int>(Grid[0].size(),-1));
-        return solution(Grid.size()-1,Grid[0].size()-1,Grid,dp);    
+        for(int i=0;i<Grid.size();++i)
+        {
+            for(int j=0;j<Grid[0].size();++j)
+            {
+                if(Grid[i][j]==1)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==0 && j==0 )
+                    dp[i][j]=1;
+                else
+                {
+                    int up=0,left=0;
+                    if(i>0)
+                        left=dp[i-1][j];
+                    if(j>0)
+                        up=dp[i][j-1];
+                    dp[i][j]=left+up;
+                }
+            }
+        }
+        return dp[Grid.size()-1][Grid[0].size()-1];
     }
 };
