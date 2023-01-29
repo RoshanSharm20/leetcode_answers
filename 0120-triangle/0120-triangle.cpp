2a05cc1fdd565ebfd1bfc9350295691ade82f15e
Roshan Sharma <roshan.s4d@gmail.com>
2023-01-29 12:29:34 +0530

    Time: 3 ms (97.74%), Space: 9.1 MB (34.62%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -13,7 +13,26 @@ public:
     }
     int minimumTotal(vector<vector<int>>& triangle) 
     {
-        vector<vector<int>> dp(triangle.size(),vector<int>(triangle.size(),-1));
-        return solution(0,0,triangle,dp);
+        // vector<vector<int>> dp(triangle.size(),vector<int>(triangle.size(),-1));
+        // return solution(0,0,triangle,dp);
+        vector<int> dp(triangle.size(),0);
+        int n=triangle.size();
+        for(int i=0;i<triangle.size();++i)
+        {
+            dp[i]=triangle[n-1][i];
+        }
+        
+        for(int i=n-2;i>=0;--i)
+        {
+            vector<int> temp(n,-1);
+            for(int j=0;j<=i;++j)
+            {
+                int down = triangle[i][j]+dp[j];
+                int diag = triangle[i][j]+dp[j+1];
+                temp[j]=min(down,diag);
+            }
+            dp=temp;
+        }
+        return dp[0];
     }
 };
