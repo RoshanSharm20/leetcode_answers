6e21375f27d9f37d0239b7235f9964297e221053
Roshan Sharma <roshan.s4d@gmail.com>
2023-02-02 19:09:32 +0530

    Time: 164 ms (30.29%), Space: 45.2 MB (31.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -21,10 +21,30 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) 
     {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int ans = solution(coins.size()-1,coins,dp,amount);
+        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,0));
+        for(int t=0;t<=amount;++t)
+        {
+            if(t%coins[0]==0)
+                dp[0][t]=t/coins[0];
+            else
+                dp[0][t]=1e9;
+        }
+        
+        for(int ind = 1;ind<coins.size();++ind)
+        {
+            for(int t=0;t<=amount;++t)
+            {
+                int nottake = dp[ind-1][t];
+                int take = 1e9;
+                if(coins[ind]<=t)
+                    take = 1 + dp[ind][t-coins[ind]];
+                dp[ind][t]=min(take,nottake);
+            }
+        }
+        int ans = dp[coins.size()-1][amount];
         if(ans>=1e9)
             return -1;
-        return ans;
+        else
+            return ans;
     }
 };
