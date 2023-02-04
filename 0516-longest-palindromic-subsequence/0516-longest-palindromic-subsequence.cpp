3aad4e04d0628806cc101bdadf0bfe3b01610019
Roshan Sharma <roshan.s4d@gmail.com>
2023-02-04 20:27:02 +0530

    Time: 226 ms (32.91%), Space: 73 MB (32.91%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -14,7 +14,18 @@ public:
     {
         string t = s;
         reverse(t.begin(),t.end());
-        vector<vector<int>> dp(s.size()+1,vector<int>(t.size()+1,-1));
-        return solution(s.size(),t.size(),s,t,dp);
+        vector<vector<int>> dp(s.size()+1,vector<int>(t.size()+1,0));
+        // return solution(s.size(),t.size(),s,t,dp);
+        for(int i=1;i<=s.size();++i)
+        {
+            for(int j=1;j<=t.size();++j)
+            {
+                if(s[i-1]==t[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[s.size()][t.size()];
     }
 };
