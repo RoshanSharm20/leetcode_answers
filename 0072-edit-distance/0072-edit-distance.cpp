286b1a1bc168fdb76603aa5cf9dd867e88304816
Roshan Sharma <roshan.s4d@gmail.com>
2023-02-06 22:40:02 +0530

    Time: 19 ms (49.09%), Space: 9 MB (41.66%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -20,7 +20,27 @@ public:
     }
     int minDistance(string word1, string word2) 
     {
-        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,-1));
-        return solution(word1.size(),word2.size(),word1,word2,dp);
+        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,0));
+        // return solution(word1.size(),word2.size(),word1,word2,dp);
+        for(int i=0;i<=word2.size();++i)
+            dp[0][i]=i;
+        for(int i=0;i<=word1.size();++i)
+            dp[i][0]=i;
+        
+        for(int index1=1;index1<=word1.size();++index1)
+        {
+            for(int index2=1;index2<=word2.size();++index2)
+            {
+                if(word1[index1-1]==word2[index2-1])
+                    dp[index1][index2]=dp[index1-1][index2-1];
+                else
+                {
+                    dp[index1][index2]=1+ min(dp[index1][index2-1],
+                                  min(dp[index1-1][index2],
+                                      dp[index1-1][index2-1]));
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
     }
 };
