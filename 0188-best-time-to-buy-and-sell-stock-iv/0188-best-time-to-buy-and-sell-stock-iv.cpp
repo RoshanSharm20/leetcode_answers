6ae24547eb2e6de86cf397773b543491133137e4
Roshan Sharma <roshan.s4d@gmail.com>
2023-02-08 09:18:26 +0530

    Time: 34 ms (30.52%), Space: 14.6 MB (21.46%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int solution(int index,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp)
+    {
+        if(cap==0 || index==prices.size())
+            return 0;
+        if(dp[index][buy][cap]!=-1)
+            return dp[index][buy][cap];
+        if(buy)
+        {
+            return dp[index][buy][cap]=max(-prices[index]+solution(index+1,0,cap,prices,dp),solution(index+1,buy,cap,prices,dp));
+        }
+        else
+        {
+            return dp[index][buy][cap]=max(prices[index]+solution(index+1,1,cap-1,prices,dp),solution(index+1,buy,cap,prices,dp));
+        }
+    }
+    int maxProfit(int k, vector<int>& prices) 
+    {
+        vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
+        return solution(0,1,k,prices,dp);
+    }
+};
+
+// class Solution {
+// public:
+//     int solution(int index,int buy,int cap,vector<int>& prices,vector<vector<vector<int>>>& dp)
+//     {
+//         if(cap==0 || index==prices.size())
+//             return 0;
+//         if(dp[index][buy][cap]!=-1)
+//             return dp[index][buy][cap];
+//         if(buy)
+//         {
+//             return dp[index][buy][cap]=max(-prices[index]+solution(index+1,0,cap,prices,dp),solution(index+1,buy,cap,prices,dp));
+//         }
+//         else
+//         {
+//             return dp[index][buy][cap]=max(prices[index]+solution(index+1,1,cap-1,prices,dp),solution(index+1,buy,cap,prices,dp));
+//         }
+//     }
+//     int maxProfit(vector<int>& prices) 
+//     {
+//         vector<vector<vector<int>>> dp(prices.size(),vector<vector<int>>(2,vector<int>(3,-1)));
+//         return solution(0,1,2,prices,dp);
+//     }
+// };
