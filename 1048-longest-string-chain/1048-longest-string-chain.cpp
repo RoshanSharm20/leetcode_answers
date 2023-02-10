4355b662b5e01a535354ece661d214a66228bc4f
Roshan Sharma <roshan.s4d@gmail.com>
2023-02-10 15:38:43 +0530

    Time: 113 ms (73.31%), Space: 13.3 MB (68.20%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    static bool myCmp(string& s1,string& s2)
+    {
+        return s1.size()<s2.size();
+    }
+    bool checkPossible(string& s1,string& s2)
+    {
+        if(s1.size()!=s2.size()+1)
+            return false;
+        int first=0,second=0;
+        while(first<s1.size())
+        {
+            if(s1[first]==s2[second])
+            {
+                first++;
+                second++;
+            }
+            else
+            {
+                first++;
+            }
+        }
+        
+        if(first==s1.size() && second==s2.size())
+            return true;
+        return false;
+    }
+    int longestStrChain(vector<string>& words) 
+    {
+        sort(words.begin(),words.end(),myCmp);
+        int n = words.size();
+        vector<int> dp(n,1);
+        int maxi=1;
+        for(int i=0;i<n;++i)
+        {
+            for(int j=0;j<i;++j)
+            {
+                if(checkPossible(words[i],words[j]) && 1+dp[j]>dp[i])
+                {
+                    dp[i]=1+dp[j];
+                }
+            }
+            maxi=max(maxi,dp[i]);
+        }
+        return maxi;
+    }
+};
