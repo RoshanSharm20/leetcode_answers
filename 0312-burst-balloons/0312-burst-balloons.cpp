c648b2a6b03ca7e48b5a3e62fa13e90af1b2e1a8
Roshan Sharma <roshan.s4d@gmail.com>
2023-02-11 12:02:16 +0530

    Time: 419 ms (69.21%), Space: 10.4 MB (63.73%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -19,7 +19,21 @@ public:
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return solution(1,n-2,nums,dp);
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        // return solution(1,n-2,nums,dp);
+        for(int i=n-2;i>=1;--i)
+        {
+            for(int j=i;j<n-1;++j)
+            {
+                int maxi = -1e9;
+                for(int k=i;k<=j;++k)
+                {
+                    int coins = nums[i-1]*nums[k]*nums[j+1] +dp[i][k-1] + dp[k+1][j];
+                    maxi = max(coins,maxi);
+                }
+                dp[i][j]=maxi;
+            }
+        }
+        return dp[1][n-2];
     }
 };
