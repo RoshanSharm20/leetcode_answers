adabb9b7a4b42540ac1cca7edd50ae5193787407
Roshan Sharma <roshan.s4d@gmail.com>
2023-02-24 00:16:43 +0530

    Time: 15 ms (62.59%), Space: 15.9 MB (85.04%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,28 +2,48 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) 
     {
-        sort(nums.begin(),nums.end());
-        vector<int> ans;
-        int count=1;
-        int ele=nums[0];
-        for(int i=1;i<nums.size();++i)
+        int val1=nums[0],count1=1,val2=nums[0],count2=0;
+        int i=1;
+        while(i<nums.size())
         {
-            if(nums[i]==ele)
-                count++;
-            else if(count>(nums.size()/3))
-            {
-                ans.push_back(ele);
-                ele=nums[i];
-                count=1;
-            }
+            if(nums[i]==val1)
+                count1++;
+            else if(nums[i]==val2)
+                count2++;
             else
             {
-                ele=nums[i];
-                count=1;
+                if(count1==0)
+                {
+                    val1=nums[i];
+                    count1=1;
+                }
+                else if(count2==0)
+                {
+                    val2=nums[i];
+                    count2=1;
+                }
+                else
+                {
+                    count1--;
+                    count2--;
+                }
             }
+            i++;
         }
-        if(count>(nums.size()/3))
-            ans.push_back(ele);
+        int n = nums.size();
+        int count_1=0,count_2=0;
+        for(int i=0;i<nums.size();++i)
+        {
+            if(nums[i]==val1)
+                count_1++;
+            else if(nums[i]==val2)
+                count_2++;
+        }
+        vector<int> ans;
+        if(count_1>(n/3))
+            ans.push_back(val1);
+        if(count_2>(n/3))
+            ans.push_back(val2);
         return ans;
     }
 };
