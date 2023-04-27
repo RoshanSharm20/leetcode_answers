afe791fdc3c6fca50ad715694498425f3b6e3356
Roshan Sharma <roshan.s4d@gmail.com>
2023-04-27 18:40:25 +0530

    Time: 0 ms (100.00%), Space: 6 MB (9.91%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // int factors(int n)
+    // {
+    //     int count=0;
+    //     for(int i=1;i<=sqrt(n);++i)
+    //     {
+    //         //lets calculate the number of factors 
+    //         if(n%i==0)
+    //         {
+    //             if(n/i==i)//to check only for unique factors
+    //                 count++;
+    //             else
+    //                 count+=2;
+    //         }
+    //     }
+    //     return count;
+    // }
+    int bulbSwitch(int n) 
+    {
+        return(sqrt(n));
+    }
+};
